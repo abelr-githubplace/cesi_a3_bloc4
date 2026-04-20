3c6f06f40c37b5a14754fdca08244cf3850abb42
Ibrahima Diarra <ibrahimakoniba13@gmail.com>
2026-04-20 11:09:48 +0200

    feat: add logger and status

---
+++ logger/src/BackupState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EasyLog
+{
+    public class BackupState
+    {
+        public string BackupName { get; set; }
+        public DateTime LastActionTime { get; set; }
+        public string Status { get; set; }  // Active or / Inactive
+
+        public int TotalFiles { get; set; }
+        public long TotalSize { get; set; }
+
+        public int FilesRemaining { get; set; }
+        public long SizeRemaining { get; set; }
+
+        public int Progress { get; set; } // en %
+
+        public string CurrentSourceFile { get; set; }
+        public string CurrentTargetFile { get; set; }
+    }
+}
---
+++ logger/src/LogEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EasyLog
+{
+    public class LogEntry
+    {
+        public DateTime Timestamp { get; set; }
+        public string BackupName { get; set; }
+        public string SourcePath { get; set; }
+        public string TargetPath { get; set; }
+        public long FileSize { get; set; }
+        public long TransferTimeMs { get; set; }
+    }
+}
