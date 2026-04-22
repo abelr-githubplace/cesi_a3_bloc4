299be1aa53c070d84861978df344f269367638d0
Abel Roffiaen <roffiaen.abel@gmail.com>
2026-04-22 17:10:05 +0200

    fix: rename cli directory

--- cli/src/Program.cs
+++ cli/src/Program.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Globalization;
 using System.Threading;
 using EasySave.lang;
-using Save; // Namespace de votre bibliothèque EasySaveLibrary
 
 namespace EasySave.CLI
 {
