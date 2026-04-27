d2c64fe847f415e14b7f1d82939739aa0b65294b
Ibrahima Diarra <ibrahimakoniba13@gmail.com>
2026-04-27 12:27:47 +0200

    feat: add xml logging support

---
+++ logger/LogFormat.cs
@@ -0,0 +1,8 @@
+﻿namespace EasyLog
+{
+    public enum LogFormat
+    {
+        Text,
+        Xml
+    }
+}
--- logger/src/Logger.cs
+++ logger/src/Logger.cs
@@ -9,8 +9,8 @@ namespace EasyLog
         public required string SaveName { get; init; }
         public required string SourceFile { get; init; }
         public required string DestinationFile { get; init; }
-        public required long FileSize { get; init; }
-        public required int TransferTime { get; init; } // ms
+        public required long FileSize { get; init; }      // kB
+        public required int TransferTime { get; init; }   // ms
     }
 
     public class Logger
@@ -19,13 +19,16 @@ namespace EasyLog
         private static readonly object s_lock = new object();
 
         private readonly string _outputFile;
+        private readonly LogFormat _format;
+        private bool _xmlOpened = false;
 
-        private Logger(string outputFile)
+        private Logger(string outputFile, LogFormat format)
         {
             _outputFile = outputFile;
+            _format = format;
         }
 
-        public static Logger Get(string outputFile)
+        public static Logger Get(string outputFile, LogFormat format = LogFormat.Text)
         {
             if (s_instance == null)
             {
@@ -33,7 +36,7 @@ namespace EasyLog
                 {
                     if (s_instance == null)
                     {
-                        s_instance = new Logger(outputFile);
+                        s_instance = new Logger(outputFile, format);
                     }
                 }
             }
@@ -42,16 +45,56 @@ namespace EasyLog
 
         public void Log(LogInfo logInfo)
         {
-            using (StreamWriter writer = new StreamWriter(_outputFile, true))
-            {
-                string line =
-                    $"[{logInfo.DateTime:dd-MM-yyyy HH:mm:ss}] " +
-                    $"{logInfo.SaveName} > SAVE from " +
-                    $"[{logInfo.SourceFile}] to " +
-                    $"[{logInfo.DestinationFile}] " +
-                    $"({logInfo.FileSize}kB) in {logInfo.TransferTime}ms";
+            if (_format == LogFormat.Xml)
+                WriteXml(logInfo);
+            else
+                WriteText(logInfo);
+        }
 
-                writer.WriteLine(line);
+        // FORMAT TEXTE  
+        private void WriteText(LogInfo logInfo)
+        {
+            using StreamWriter writer = new StreamWriter(_outputFile, true);
+
+            string line =
+                $"[{logInfo.DateTime:dd-MM-yyyy HH:mm:ss}] " +
+                $"{logInfo.SaveName} > SAVE from " +
+                $"[{logInfo.SourceFile}] to " +
+                $"[{logInfo.DestinationFile}] " +
+                $"({logInfo.FileSize}kB) in {logInfo.TransferTime}ms";
+
+            writer.WriteLine(line);
+        }
+
+        // FORMAT XML  
+        private void WriteXml(LogInfo logInfo)
+        {
+            using StreamWriter writer = new StreamWriter(_outputFile, true);
+
+            if (!_xmlOpened)
+            {
+                writer.WriteLine("<Logs>");
+                _xmlOpened = true;
+            }
+
+            writer.WriteLine("  <LogEntry>");
+            writer.WriteLine($"    <Date>{logInfo.DateTime:dd-MM-yyyy HH:mm:ss}</Date>");
+            writer.WriteLine($"    <SaveName>{logInfo.SaveName}</SaveName>");
+            writer.WriteLine($"    <Source>{logInfo.SourceFile}</Source>");
+            writer.WriteLine($"    <Target>{logInfo.DestinationFile}</Target>");
+            writer.WriteLine($"    <SizeKB>{logInfo.FileSize}</SizeKB>");
+            writer.WriteLine($"    <TransferTimeMS>{logInfo.TransferTime}</TransferTimeMS>");
+            writer.WriteLine("  </LogEntry>");
+        }
+
+        //  FERMETURE XML
+        public void Close()
+        {
+            if (_format == LogFormat.Xml && _xmlOpened)
+            {
+                using StreamWriter writer = new StreamWriter(_outputFile, true);
+                writer.WriteLine("</Logs>");
+                _xmlOpened = false;
             }
         }
     }
