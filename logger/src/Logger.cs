2ac5e436e7d22b59db886e544956f0f5e3121236
Ibrahima Diarra <ibrahimakoniba13@gmail.com>
2026-04-27 15:42:32 +0200

    fix : adjust XML log

--- logger/src/Logger.cs
+++ logger/src/Logger.cs
@@ -9,8 +9,8 @@ namespace EasyLog
         public required string SaveName { get; init; }
         public required string SourceFile { get; init; }
         public required string DestinationFile { get; init; }
-        public required long FileSize { get; init; }      // kB
-        public required int TransferTime { get; init; }   // ms
+        public required long FileSize { get; init; }
+        public required int TransferTime { get; init; }
     }
 
     public class Logger
@@ -20,7 +20,6 @@ namespace EasyLog
 
         private readonly string _outputFile;
         private readonly LogFormat _format;
-        private bool _xmlOpened = false;
 
         private Logger(string outputFile, LogFormat format)
         {
@@ -46,12 +45,16 @@ namespace EasyLog
         public void Log(LogInfo logInfo)
         {
             if (_format == LogFormat.Xml)
+            {
                 WriteXml(logInfo);
+            }
             else
+            {
                 WriteText(logInfo);
+            }
         }
 
-        // FORMAT TEXTE  
+        //  FORMAT TEXTE (par défaut)
         private void WriteText(LogInfo logInfo)
         {
             using StreamWriter writer = new StreamWriter(_outputFile, true);
@@ -66,36 +69,25 @@ namespace EasyLog
             writer.WriteLine(line);
         }
 
-        // FORMAT XML  
+        // FORMAT XML 
         private void WriteXml(LogInfo logInfo)
         {
             using StreamWriter writer = new StreamWriter(_outputFile, true);
 
-            if (!_xmlOpened)
-            {
-                writer.WriteLine("<Logs>");
-                _xmlOpened = true;
-            }
-
-            writer.WriteLine("  <LogEntry>");
-            writer.WriteLine($"    <Date>{logInfo.DateTime:dd-MM-yyyy HH:mm:ss}</Date>");
-            writer.WriteLine($"    <SaveName>{logInfo.SaveName}</SaveName>");
-            writer.WriteLine($"    <Source>{logInfo.SourceFile}</Source>");
-            writer.WriteLine($"    <Target>{logInfo.DestinationFile}</Target>");
-            writer.WriteLine($"    <SizeKB>{logInfo.FileSize}</SizeKB>");
-            writer.WriteLine($"    <TransferTimeMS>{logInfo.TransferTime}</TransferTimeMS>");
-            writer.WriteLine("  </LogEntry>");
+            writer.WriteLine(
+                $"<Date>{logInfo.DateTime:dd-MM-yyyy HH:mm:ss}</Date>" +
+                $"<Name>{logInfo.SaveName}</Name>" +
+                $"<Source>{logInfo.SourceFile}</Source>" +
+                $"<Target>{logInfo.DestinationFile}</Target>" +
+                $"<SizeKB>{logInfo.FileSize}</SizeKB>" +
+                $"<TransferTimeMS>{logInfo.TransferTime}</TransferTimeMS>"
+            );
         }
 
-        //  FERMETURE XML
+        // Close volontairement 
         public void Close()
         {
-            if (_format == LogFormat.Xml && _xmlOpened)
-            {
-                using StreamWriter writer = new StreamWriter(_outputFile, true);
-                writer.WriteLine("</Logs>");
-                _xmlOpened = false;
-            }
+            // format journal sans racine
         }
     }
 }
