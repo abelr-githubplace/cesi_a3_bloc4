4244fe35e05cc1f5fb79def6b3733367e376ff90
Sixela1702 <alexis.bonnetethgen@viacesi.fr>
2026-04-28 11:31:47 +0200

    fix: five max unit test

--- cli/tests/UnitTest.cs
+++ cli/tests/UnitTest.cs
@@ -2,28 +2,130 @@
 using SaveManager;
 using EasySaveConsole;
 using System.Collections.Generic;
+using System;
+using System.IO;
 
 namespace EasySaveConsole.Tests
 {
     [TestClass]
-    public class TestCLI
+    [DoNotParallelize]
+    public class SaveInfosContextTests
     {
-        [TestMethod]
-        public void SaveInfosContext_ShouldIgnoreZero_And_CorrectlyAlignIndexes()
+        private List<SaveInfo> _existingSaves = new();
+
+        [TestInitialize]
+        public void Setup()
         {
-            var existingSaves = new List<SaveInfo>
+            _existingSaves.Clear();
+            _existingSaves.Add(new SaveInfo { SaveId = 1, SaveName = "Save1", SourcePath = "C:\\src1", DestinationPath = "D:\\dst1" });
+            _existingSaves.Add(new SaveInfo { SaveId = 2, SaveName = "Save2", SourcePath = "C:\\src2", DestinationPath = "D:\\dst2" });
+        }
+
+        [TestMethod]
+        public void SaveInfosContext_ValidExistingIds_ShouldReturnExistingSaves()
+        {
+            var userInputs = new List<int> { 1, 2 };
+            var consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+
+            try
             {
-                new SaveInfo { SaveId = 1, SaveName = "SauvegardeA", SourcePath = "C:\\A", DestinationPath = "D:\\A" },
-                new SaveInfo { SaveId = 2, SaveName = "SauvegardeB", SourcePath = "C:\\B", DestinationPath = "D:\\B" }
-            };
+                var result = App.SaveInfosContext(userInputs, _existingSaves);
 
-            var userInputs = new List<int> { 0, 1, 2 };
+                Assert.AreEqual(2, result.Length);
+                Assert.AreEqual("Save1", result[0].SaveName);
+                Assert.AreEqual("Save2", result[1].SaveName);
+            }
+            finally
+            {
+                RestoreConsole();
+            }
+        }
 
-            var result = App.SaveInfosContext(userInputs, existingSaves);
+        [TestMethod]
+        public void SaveInfosContext_InvalidIds_ShouldPromptUntilValid()
+        {
+            var userInputs = new List<int> { 0, 6 };
 
-            Assert.AreEqual(2, result.Length); 
-            Assert.AreEqual("SauvegardeA", result[0].SaveName); 
-            Assert.AreEqual("SauvegardeB", result[1].SaveName); 
+            var consoleInput = new StringReader("1\n2\n");
+            Console.SetIn(consoleInput);
+
+            var consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+
+            try
+            {
+                var result = App.SaveInfosContext(userInputs, _existingSaves);
+
+                Assert.AreEqual(2, result.Length);
+                Assert.AreEqual("Save1", result[0].SaveName);
+                Assert.AreEqual("Save2", result[1].SaveName);
+            }
+            finally
+            {
+                RestoreConsole();
+            }
+        }
+
+        [TestMethod]
+        public void SaveInfosContext_NewSaveId_ShouldPromptForDetailsAndCreateSave()
+        {
+            var userInputs = new List<int> { 3 };
+
+            var simulatedInput = "NouvelleSave\nC:\\NouveauDossier\\Src\nD:\\NouveauDossier\\Dst\n";
+            var consoleInput = new StringReader(simulatedInput);
+            Console.SetIn(consoleInput);
+
+            var consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+
+            try
+            {
+                var result = App.SaveInfosContext(userInputs, _existingSaves);
+
+                Assert.AreEqual(1, result.Length);
+                Assert.AreEqual(3u, result[0].SaveId);
+                Assert.AreEqual("NouvelleSave", result[0].SaveName);
+            }
+            finally
+            {
+                RestoreConsole();
+            }
+        }
+
+        [TestMethod]
+        public void SaveInfosContext_EmptyInputsWhileCreating_ShouldPromptUntilNotEmpty()
+        {
+            var userInputs = new List<int> { 3 };
+
+            var simulatedInput = "   \n\nValidName\n\nC:\\Src\nD:\\Dst\n";
+            var consoleInput = new StringReader(simulatedInput);
+            Console.SetIn(consoleInput);
+
+            var consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+
+            try
+            {
+                var result = App.SaveInfosContext(userInputs, _existingSaves);
+
+                Assert.AreEqual("ValidName", result[0].SaveName);
+                Assert.AreEqual("C:\\Src", result[0].SourcePath);
+            }
+            finally
+            {
+                RestoreConsole();
+            }
+        }
+
+        private void RestoreConsole()
+        {
+            var standardIn = new StreamReader(Console.OpenStandardInput());
+            Console.SetIn(standardIn);
+
+            var standardOut = new StreamWriter(Console.OpenStandardOutput());
+            standardOut.AutoFlush = true;
+            Console.SetOut(standardOut);
         }
     }
 }
