c21db475242ec3e12df7b73d03616051f5be274b
Sixela1702 <alexis.bonnetethgen@viacesi.fr>
2026-05-11 17:21:47 +0200

    feat: change GUI colors

---
+++ gui/src/ViewModel/GuiProgressBar.cs
@@ -0,0 +1,22 @@
+﻿using Observer;
+
+namespace EasySave.GUI.ViewModels
+{
+    public class GuiProgressBar : ISubscriber
+    {
+        private SaveJob _job;
+        private Saver.Progress _progressTracker;
+
+        public GuiProgressBar(SaveJob job, Saver.Progress progressTracker)
+        {
+            _job = job;
+            _progressTracker = progressTracker;
+            _progressTracker.Subscribe(this);
+        }
+
+        public void Update()
+        {
+            _job.Progress = _progressTracker.GetProgress();
+        }
+    }
+}
